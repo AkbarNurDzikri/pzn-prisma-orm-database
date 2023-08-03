0d6fd3dfb2e7e3e458260f1ce032d4101d36f2f5
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-03 15:42:36 +0700

    Try connecting to database

---
+++ tests/prisma-client.test.js
@@ -0,0 +1,10 @@
+import {PrismaClient} from '@prisma/client';
+
+describe('Prisma client', () => {
+  it('should be able to connect to database', async () => {
+    const prisma = new PrismaClient;
+    await prisma.$connect();
+
+    await prisma.$disconnect();
+  });
+});
