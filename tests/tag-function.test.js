f98a7056066d6aee3bab388269b8154cc70003df
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-04 10:13:17 +0700

    Contoh SQL Injection yang dihindari menggunakan tag function yang digunakan oleh prisma

--- tests/tag-function.test.js
+++ tests/tag-function.test.js
@@ -9,3 +9,10 @@ test('tag function', () => {
   tagFunction `Hello ${name}, how are you?`;
   tagFunction `Bye ${name}, see you later`;
 });
+
+test('menghindari SQL Injection dengan tag function javascript yang digunakan oleh prisma', () => {
+  const name = "Dzikri'; DROP TABLE users;";
+  const age = 29;
+
+  tagFunction `SELECT * FROM users WHERE name = ${name} AND age = ${age}`;
+});
