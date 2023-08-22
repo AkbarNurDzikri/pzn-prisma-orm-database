11f651e872aacd2def39503318807e353cb069d3
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-22 10:32:45 +0700

    Create products table and prisma model

--- database.sql
+++ database.sql
@@ -14,3 +14,20 @@ CREATE TABLE customers(
   CONSTRAINT customers_email_unique UNIQUE(email),
   CONSTRAINT customers_phone_unique UNIQUE(phone)
 ) ENGINE INNODB;
+
+CREATE TABLE products(
+  id VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  price INT NOT NULL,
+  stock INT NOT NULL,
+  category VARCHAR(100),
+
+  PRIMARY KEY (id)  
+) ENGINE INNODB;
+
+INSERT INTO products(id, name, price, stock, category)
+  VALUES('P0001', 'A', 1000, 100, 'K1'),
+    ('P0002', 'B', 2000, 200, 'K2'),
+    ('P0003', 'C', 3000, 300, 'K3'),
+    ('P0004', 'D', 4000, 400, 'K4'),
+    ('P0005', 'E', 5000, 500, 'K5');
