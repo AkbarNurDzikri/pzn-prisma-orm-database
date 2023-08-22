43000a5681c6b95a9e0c9a81172b587def6e1f34
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-22 10:14:52 +0700

    select fields with prisma ORM

--- tests/selectField.test.js
+++ tests/selectField.test.js
@@ -1,20 +1,36 @@
 import {prismaClient} from '../src/prisma-client.js';
 
 describe("Prisma Client", () => {
-  it("should can create and select fields", async ()=> {
-    const customer = await prismaClient.customer.create({
-      data: {
-        id: "18",
-        name: "Megawati",
-        email: "mega@gmail.pdi",
-        phone: "25345626",
-      },
+  // it("should can create and select fields", async ()=> {
+  //   const customer = await prismaClient.customer.create({
+  //     data: {
+  //       id: "18",
+  //       name: "Megawati",
+  //       email: "mega@gmail.pdi",
+  //       phone: "25345626",
+  //     },
+  //     select: {
+  //       id: true,
+  //       name: true,
+  //     }
+  //   });
+
+  //   console.table(customer);
+  // });
+
+  it("should can select fields", async () => {
+    const customers = await prismaClient.customer.findMany({
       select: {
         id: true,
         name: true,
       }
     });
 
-    console.table(customer);
+    for (const customer of customers) {
+      expect(customer.id).toBeDefined();
+      expect(customer.name).toBeDefined();
+      expect(customer.email).toBeUndefined()
+      expect(customer.phone).toBeUndefined();
+    }
   });
 });
